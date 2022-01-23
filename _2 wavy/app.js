a595ebf51bf8d8a0454d6335fb29f07d3d5923bc
Jackson Haskamp <jacksonhaskamp@gmail.com>
2022-01-23 00:28:44 -0600

    tried to work on ajax, need to look at matrix3d instead i think
    added jquery and tried to use ajax to change direction of the scrolling project header bar based on scrolling up or down.

--- _2 wavy/app.js
+++ _2 wavy/app.js
@@ -1,16 +1,59 @@
 var oldValue = 0
 var newValue = 0
-window.addEventListener('scroll', (e) => {
-  newValue = window.pageYOffset;
-  if (oldValue < newValue) {
-    var document.getElementById("project-ticker");
+//window.addEventListener('scroll', (e) => {
+//  newValue = window.pageYOffset;
+//  if (oldValue < newValue) {
+//    var tickbox = document.getElementById("project-ticker");
+//      
+//      tickbox.style.animationName = "ticker-reverse";
+//      
+//    
+//  } else if (oldValue > newValue) {
+//    console.log("Down");
+//      var tickbox = document.getElementById("project-ticker");
+//      
+//      tickbox.style.animationName = "ticker";
+//  }
+//  oldValue = newValue;
+//});
+$.ajax({
+    success: function() {
+        console.log("Ajax!!");
+      newValue = window.pageYOffset;
+  var tickbox = document.getElementById("project-ticker");
+
+    if (oldValue < newValue) {
+        console.log("Up");
+        tickbox.style.animationName
       
     
   } else if (oldValue > newValue) {
     console.log("Down");
+      
+      tickbox.style.animationName = "ticker";
   }
   oldValue = newValue;
+        
+    }
 });
+
+window.onscroll = function (e) {  
+// called when the window is scrolled.  
+  newValue = window.pageYOffset;
+  var tickbox = document.getElementById("project-ticker");
+
+    if (oldValue < newValue) {
+        console.log("Up");
+        tickbox.style.animationName
+      
+    
+  } else if (oldValue > newValue) {
+    console.log("Down");
+      
+      tickbox.style.animationName = "ticker";
+  }
+  oldValue = newValue;
+} 
 
 //function scrollChanger() {
 //    
